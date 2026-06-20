fa0125315cadb38b102074b4b428b930182ab97d
Muhammad Umar <umar@example.com>
2026-06-20 09:06:54 +0500

    PWA complete - Install button ready

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('umar-app').then((cache) => cache.addAll(['/'])));
+  e.waitUntil(caches.open('umar-app').then((cache) => >));
 });
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then((res) => >));
 });
